967d0076b972091eddcd6f347aa67c7b6413b630
Kuwunak <ulysse.hubert.uh@gmail.com>
2026-04-21 15:36:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ from flask import Flask, render_template, send_from_directory, request
 
 app = Flask("petitions")
 
-NOMBRE_DE_SIGNATURES = 0
-
+NB_SIGNATURES = 0
 
 @app.get("/")
 def index():
-    return render_template("index.html", nombre_signatures=NOMBRE_DE_SIGNATURES)
-
-
-@app.post("/")
-def sign():
-    global NOMBRE_DE_SIGNATURES
-    NOMBRE_DE_SIGNATURES += 1
-    print(request.form)
+    return render_template("index.html", nombre_signatures=NB_SIGNATURES)
 
 
 @app.get("/style.css")
 def sytle():
     return send_from_directory("static", "style.css")
+
+@app.post("/")
+def sign_petition():
+    global NB_SIGNATURES
+    NB_SIGNATURES += 1
+    return render_template("sign.html", name=request.form["name"])
